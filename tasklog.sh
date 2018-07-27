150fdda5b35a82aa0aa4107d6d1996c89f565fc2
Dbandfield <daniel.bandfield"ntlworld.com>
2018-07-27 21:05:25 +0100

    added install commands

--- tasklog.sh
+++ tasklog.sh
@@ -2,6 +2,19 @@
 
 set -eo pipefail
 
+function global_install
+{
+    pip install virtualenv
+    virtualenv venv
+}
+
+function local_install
+{
+    source venv/bin/activate
+    pip install -r requirements.txt
+    deactivate
+}
+
 function test
 {
     source venv/bin/activate
